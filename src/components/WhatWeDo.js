10c0c8e01f38db325e1e044e9070d4b2657e90f5
dannyr22 <dannyr2763@hotmail.com>
2021-03-05 11:23:17 +0000

    what we do component created

---
+++ src/components/WhatWeDo.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import drink1 from '../img/drink1.jpg';
+
+const WhatWeDo = () => {
+  return (
+    <div>
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2>We Bring <span>Cocktails</span></h2>
+          </div>
+          <div className="hide">
+            <h2>Straight To Your</h2>
+          </div>
+          <div className="hide">
+            <h2><span>Door</span></h2>
+          </div>
+        </div>
+        <p>To see a list of what we sell then please click the button</p>
+        <button>Menu</button>
+      </div>
+      <div className="image">
+       <img src={drink1} alt=""/>
+      </div>
+    </div>
+  )
+}
+
+export default WhatWeDo
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import WhatWeDo from '../components/WhatWeDo'
+
+const AboutUs = () => {
+  return (
+    <div>
+      <WhatWeDo />
+    </div>
+  )
+}
+
+export default AboutUs
