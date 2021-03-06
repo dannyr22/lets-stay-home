e3051d894f782d7760d49eced7c4a2ae3008db05
dannyr22 <dannyr2763@hotmail.com>
2021-03-06 19:55:11 +0000

    form edited in html

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -13,15 +13,14 @@ const GetInTouch = () => {
       <ImagePour>
         <img src={cocktailPour} alt="cocktail being poured"/>
       </ImagePour>
-   
-          <form   netlify name="contact v1" action="POST" data-netlify="true" onSubmit="submit">
-            <input type="hidden" name="form-name" value="contact v1"/>
-            <input type="text" placeholder="Name" name="name"/>
-            <input type="email" id="e-mail" placeholder="Email Address" name="email"/>
-            <textarea name="request" id="" cols="30" rows="10"></textarea>
-            <button type="submit">Submit</button>
-          </form>
-       
+        <Form
+          name="contact" action="/contact" method="POST" data-netlify="true" onSubmit="submit">
+          <input type="hidden" name="form-name" value="contact v1"/>
+          <input required type="text" placeholder="Name" name="name"/>
+          <input required type="email" placeholder="Email Address" name="email"/>
+          <textarea required name="request" id="" cols="30" rows="10"></textarea>
+          <button type="submit">Send</button>
+        </Form>
       </FormContainer>
       
     </div>
@@ -36,37 +35,37 @@ img {
 }
 ;`
 
-// const Form = styled.form` 
-// display: flex;
-// flex-direction: column;
-// padding: 5rem 0rem;
-// margin-right: 10rem;
+const Form = styled.form` 
+display: flex;
+flex-direction: column;
+padding: 5rem 0rem;
+margin-right: 10rem;
 
-// input, textarea {
-//   border: none;
-//   width: 30rem;
-//   outline: none;
-//   padding: 1rem 2rem;
-//   margin-top: 1rem;
-//   border-radius: 10px;
-//   box-shadow: 2px 2px 5px rgba(0,0,0,0.5);
-//   font-size: 1rem;
-//   background: rgba(255,255,255);
-//   transition: all 0.35s ease-in-out;
-//   &:focus {
-//     border: 5px solid rgba(106,194,128,0.7);
-//     color: #504c4c;
-//     background: white;
+input, textarea {
+  border: none;
+  width: 30rem;
+  outline: none;
+  padding: 1rem 2rem;
+  margin-top: 1rem;
+  border-radius: 10px;
+  box-shadow: 2px 2px 5px rgba(0,0,0,0.5);
+  font-size: 1rem;
+  background: rgba(255,255,255);
+  transition: all 0.35s ease-in-out;
+  &:focus {
+    border: 5px solid rgba(106,194,128,0.7);
+    color: #504c4c;
+    background: white;
 
-//   }
-// }
+  }
+}
 
-// button {
-//   margin-top: 1rem;
-//   width: 30rem;
-// }
+button {
+  margin-top: 1rem;
+  width: 30rem;
+}
 
-// `
+`
 
 const FormContainer = styled.div` 
 display: flex;
