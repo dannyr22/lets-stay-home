e72cd738340f15d870d8abb2b5c673730064a963
dannyr22 <dannyr2763@hotmail.com>
2021-03-09 16:51:48 +0000

    ref updated

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -23,8 +23,8 @@ const Gallery = () => {
     <Title>
     <h2 >Gallery</h2>
       </Title>
-      <div></div>
-    <GalleryStyle className="gallery" ref={element} variants={fadeUp} initial="hidden" animate={controls}>
+      <div>ref={element} </div>
+    <GalleryStyle className="gallery" variants={fadeUp} initial="hidden" animate={controls}>
       <Slideshow >
         <img src={cocktails} alt="cocktails"/>
         <img src={espresso} alt="espresso"/>
